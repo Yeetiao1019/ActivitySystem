2aeafbda45f4282d9df3babda848a42a3409010d
YeeTiao1019 <n1533512@gmail.com>
2022-01-31 23:38:11 +0800

    修正 FullName 為空值時會顯示空白, 修改用戶資料頁面的用戶名稱欄位設定為必填

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -34,6 +34,7 @@ namespace ActivitySystem.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [Required]
             [Display(Name = "用戶名稱")]
             public string FullName { get; set; }
 
